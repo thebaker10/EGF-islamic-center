4bf1a7c8d8a7598c2c44e83c01d27f596cfe7996
Mohammed Alkhafaji <moahmmed199898@users.noreply.github.com>
2021-07-25 17:25:20 -0400

    Delete src directory

--- src/script/script.ts
+++
@@ -1,7 +0,0 @@
-// import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
-// import "@fortawesome/fontawesome-free/css/solid.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import { IndexFunctions } from "./index";
-
-IndexFunctions();
-
