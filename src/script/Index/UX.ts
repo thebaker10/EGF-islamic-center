8aac80d7905b70e029045956809d6e760197f390
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2021-08-05 14:53:28 -0400

    fixing a few issues with home section and snapping

--- src/script/Index/UX.ts
+++ src/script/Index/UX.ts
@@ -1,10 +1,7 @@
 import Calendar from "./Calendar";
 
-
-export function toggleNavbar() {  
-    const prayerSection = document.getElementById("prayer_section");
-    const servicesSection = document.getElementById("services");
-    const homeSection = document.getElementById("home");
+export function toggleNavbar(){
+    const sections = document.querySelectorAll("section");
     const navBar = document.querySelector("nav");
     const hideNavBar = ()=>navBar.classList.add("hidden");
     const showNavBar = ()=>navBar.classList.remove("hidden");
@@ -13,14 +10,20 @@ export function toggleNavbar() {
         rootMargin: "0px",
         threshold: 1.0,
     }
-
-    new IntersectionObserver(showNavBar ,options).observe(prayerSection);
-    new IntersectionObserver(hideNavBar, options).observe(servicesSection);
-    new IntersectionObserver(hideNavBar, options).observe(homeSection);
-
+    for(let i = 0; i < sections.length; i++) {
+        if(i < 2) {
+            new IntersectionObserver(hideNavBar, options).observe(sections[i]);
+        } else {
+            new IntersectionObserver(showNavBar, options).observe(sections[i]);
+        }
+    }
+    hideNavBar();
+    
 }
 
 export function initCalendar() {
     const calendar = new Calendar();
     calendar.init();
 }
+
+
