bf3df5f188c8623ed568cbafa50789eb98a9f387
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2021-08-10 13:30:27 -0400

    removing extra js code

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,9 +1,3 @@
-// import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
-// import "@fortawesome/fontawesome-free/css/solid.min.css";
-// import "@fortawesome/fontawesome-free/css/brands.min.css";
-
 import { IndexFunctions } from "./Index/index";
 
 IndexFunctions();
-
-console.log("hello")
